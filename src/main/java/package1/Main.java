40546a30e2cfb4d02b38c91216850b5f55eaa5ec
denis-shvetcov <deniska28082001@gmail.com>
2020-02-18 04:13:47 +0700

    main changed

--- src/main/java/package1/Main.java
+++ src/main/java/package1/Main.java
@@ -5,49 +5,7 @@ import java.util.Map;
 import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) {
-        Address ad1 = new Address("Ленина","15","33");
-        Address ad2 = new Address("Ленина","1","12");
-        Address ad3 = new Address("Ленина","15","22");
-        Address ad4 = new Address("Кировская","23","1");
-        Address ad5 = new Address("Кировская","15","33");
-        Address ad6 = new Address("Кировская","2","66");
-        Address ad7 = new Address("Кедровая","33","33");
-        Address ad8 = new Address("Садовая","15","12");
-        Map<String,Address> book = new HashMap<>();
-        book.put("Сачин",ad1);
-        book.put("Кислевев",ad2);
-        book.put("Постин",ad3);
-        book.put("Иванов",ad4);
-        book.put("Петров",ad5);
-        book.put("Адик",ad6);
-        book.put("Сома",ad7);
-        book.put("Ручкин",ad8);
-        AddressBook addressBook = new AddressBook(book);
 
-        Set<String> set = addressBook.getPeople("Ленина","15");
-        for (String el: set
-             ) {
-            System.out.println(el);
-        }
-        Address newad = new Address("Кукуева","12","33");
-        addressBook.changeAddress("Сачин", newad);
-        set = addressBook.getPeople("Ленина","15");
-        for (String el: set
-        ) {
-            System.out.println(el);
-        }
-
-        Address newad2 = new Address("Кукуева","12","12");
-        addressBook.add("Васильков", newad2);
-        set = addressBook.getPeople("Кукуева","12");
-        for (String el: set
-        ) {
-            System.out.println(el);
-        }
-        System.out.println();
-        System.out.println();
-        addressBook.showAll();
     }
 
 }
